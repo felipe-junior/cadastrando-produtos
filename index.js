9a26a6f436024a2cd67fced90e011810c77a56bd
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-04-22 18:47:22 -0300

    Porta padrao

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import fs from 'fs'
 
 const app = express()
-
+const porta = 8080
 const index = fs.readFileSync("C:/Users/Felipe/Documents/NodeSql/aprendendo-node/index.html", "utf-8")
 app.get("/adicionar", (req, res)=>{
     res.send(index)
@@ -11,6 +11,6 @@ app.post("/adicionar", (req, res)=>{
     res.render('<h1>Enviado</h1>')
 })
 
-app.listen(8080, ()=>{
+app.listen(porta, ()=>{
     console.log('Servidor rodando');
 })
