515f0bc50cd4d038ba07d92a17496b9dd52ae488
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-04-22 20:53:13 -0300

    Servidor para receber o objeto json

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import fs from 'fs'
 
+
 const app = express()
 const porta = 8080
 const index = fs.readFileSync("C:/Users/Felipe/Documents/NodeSql/aprendendo-node/index.html", "utf-8")
+//app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.get("/adicionar", (req, res)=>{
     res.send(index)
 })
 app.post("/adicionar", (req, res)=>{
-    res.render('<h1>Enviado</h1>')
+   console.log(req.body)
+   res.send(req.body)
 })
 
 app.listen(porta, ()=>{
---
+++ salvar.js
@@ -0,0 +1,2 @@
+import sql from 'mssql'
+const config = "Server"
